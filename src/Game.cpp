c495532027817a6234e8c6cedd4750f3e002c105
eldhom <wass95@hotmail.se>
2013-11-15 23:18:19 +0100

    l2

--- src/Game.cpp
+++ src/Game.cpp
@@ -55,10 +55,6 @@ void Game::run(int players)
 	app.setView(view);
 	sf::Event			event;
 	
-	Worm				worm1, worm2;
-						worm1.init(sf::Color::Red, sf::Vector2f(100,100), 0);
-						worm2.init(sf::Color::Blue, sf::Vector2f(700, 500), 180);
-
 	this->initCommands();
 	sf::Time	elapsed_time;	
 	sf::Clock 	clock;
@@ -75,31 +71,7 @@ void Game::run(int players)
 		}
 		elapsed_time = clock.restart();
 	
-		if(CheckInput(this->commands[0])) {
-			worm1.turn(Left, elapsed_time);
-		}
-		if(CheckInput(this->commands[1])) {
-			worm1.turn(Right, elapsed_time);
-		}
-		if(CheckInput(this->commands[2])) {
-			worm2.turn(Left, elapsed_time);
-		}
-		if(CheckInput(this->commands[3])) {
-			worm2.turn(Right, elapsed_time);
-		}
-		worm1.update(elapsed_time);
-		worm2.update(elapsed_time);
-
-		if(worm1.headCollision(worm2) || worm1.tailCollision(worm2) || worm1.tailCollision(worm1)) {
-			return;
-		}
-		if(worm2.headCollision(worm1) || worm2.tailCollision(worm1) || worm2.tailCollision(worm2)) {
-			return;
-		}
-
 		app.clear();
-		app.draw(worm1);
-		app.draw(worm2);
 		app.display();
 	}
 }
