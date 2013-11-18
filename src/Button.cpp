6fe498fec791d8705fc4eb495c12aba94bd43a5a
Eldhom <eldhom@Eldhoms-Mac-mini.local>
2013-11-18 19:58:01 +0100

    l

--- src/Button.cpp
+++ src/Button.cpp
@@ -22,25 +22,40 @@ void Button::setSize(sf::Vector2f size)
 }
 
 void Button::setPosition(sf::Vector2f position)
-{}
+{
+	this->shape.setPosition(position);
+	this->text.setPosition(position);
+}
 
 void Button::setFillColor(sf::Color color)
-{}
+{
+	this->shape.setFillColor(color);
+}
 	
 void Button::setOutlineColor(sf::Color color)
-{}
+{
+	this->shape.setOutlineColor(color);
+}
 
 void Button::setOutlineThickness(float thickness)
-{}
+{
+	this->shape.setOutlineThickness(thickness);
+}
 
 void Button::setText(std::string text)
-{}
+{
+	this->text.setString(text);
+}
 
 void Button::setTextColor(sf::Color color)
-{}
+{
+	this->text.setColor(color);
+}
 	
 void Button::setActive(bool active)
-{}
+{
+	this->active = active;
+}
 
 bool Button::isActive()
 {
